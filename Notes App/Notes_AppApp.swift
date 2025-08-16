5758b089339ee2a6c5b981b2f9e73fb14d96877e
Karthikeyan <karthikeyanramasamy1210@gmail.com>
2025-08-16 20:47:31 +0530

    Initial commit

---
+++ Notes App/Notes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notes_AppApp.swift
+//  Notes App
+//
+//  Created by Karthikeyan Ramasamy on 16/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Notes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Notes AppTests/Notes_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Notes_AppTests.swift
+//  Notes AppTests
+//
+//  Created by Karthikeyan Ramasamy on 16/08/25.
+//
+
+import Testing
+@testable import Notes_App
+
+struct Notes_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
